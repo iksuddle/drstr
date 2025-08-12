62391264e29dd133a7c647221d4e1fcbfb74c585
iksuddle <iksuddle@gmail.com>
2025-08-12 23:47:57 +0400

    move to single file

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,78 @@
-pub mod error;
-mod scanner;
+use std::{iter::Peekable, str::Chars, time::Duration};
 
-use std::time::Duration;
+#[derive(thiserror::Error, Debug, PartialEq)]
+pub enum Error {
+    #[error("unexpected character: {0}")]
+    UnexpectedChar(char),
+    #[error("unexpected unit: {0}")]
+    UnexpectedUnit(String),
+    #[error("expected a unit")]
+    ExpectedUnit,
+    #[error("expected a number")]
+    ExpectedNumber,
+}
 
-use error::Error;
-use scanner::{Scanner, Token};
+#[derive(Debug, PartialEq, Eq)]
+pub enum Token {
+    Number(u32),
+    Unit(String),
+}
+
+pub struct Scanner<'a> {
+    chars: Peekable<Chars<'a>>,
+}
+
+impl<'a> Scanner<'a> {
+    pub fn new(source: &'a str) -> Self {
+        Scanner {
+            chars: source.chars().peekable(),
+        }
+    }
+
+    pub fn scan_tokens(&mut self) -> Result<Vec<Token>, Error> {
+        let mut tokens = vec![];
+
+        while let Some(c) = self.chars.peek() {
+            tokens.push(match c {
+                ' ' | '\n' | '\t' | ',' => {
+                    self.chars.next();
+                    continue;
+                }
+                '0'..='9' => Token::Number(self.scan_number()),
+                'a'..='z' | 'A'..='Z' => Token::Unit(self.scan_unit()),
+                x => return Err(Error::UnexpectedChar(*x)),
+            });
+        }
+
+        Ok(tokens)
+    }
+
+    fn scan_number(&mut self) -> u32 {
+        let mut literal = String::new();
+
+        while let Some(c) = self.chars.peek() {
+            if !c.is_ascii_digit() {
+                break;
+            }
+            literal.push(self.chars.next().unwrap());
+        }
+
+        literal.parse().unwrap()
+    }
+
+    fn scan_unit(&mut self) -> String {
+        let mut literal = String::new();
+
+        while let Some(c) = self.chars.peek() {
+            if !c.is_ascii_alphabetic() {
+                break;
+            }
+            literal.push(self.chars.next().unwrap());
+        }
+
+        literal
+    }
+}
 
 fn get_unit_duration(unit: &str) -> Result<Duration, Error> {
     let u = unit.to_lowercase();
@@ -47,37 +115,36 @@ fn parse_tokens(tokens: Vec<Token>) -> Result<Duration, Error> {
 
 #[cfg(test)]
 mod tests {
+    use crate::{Error, Scanner, Token, parse};
     use std::time::Duration;
-
-    use crate::{error::Error, parse};
 
     #[test]
     fn test_scanner() {
-        // let mut scanner = Scanner::new("10 seconds");
-        // let tokens = scanner.scan_tokens();
-        // assert_eq!(
-        //     tokens,
-        //     Ok(vec![Token::Number(10), Token::Unit("seconds".to_string())])
-        // );
-        //
-        // let mut scanner = Scanner::new("9hr1min");
-        // let tokens = scanner.scan_tokens();
-        // assert_eq!(
-        //     tokens,
-        //     Ok(vec![
-        //         Token::Number(9),
-        //         Token::Unit("hr".to_string()),
-        //         Token::Number(1),
-        //         Token::Unit("min".to_string()),
-        //     ])
-        // );
-        //
-        // let mut scanner = Scanner::new("712635 days");
-        // let tokens = scanner.scan_tokens();
-        // assert_eq!(
-        //     tokens,
-        //     Ok(vec![Token::Number(712635), Token::Unit("days".to_string())])
-        // );
+        let mut scanner = Scanner::new("10 seconds");
+        let tokens = scanner.scan_tokens();
+        assert_eq!(
+            tokens,
+            Ok(vec![Token::Number(10), Token::Unit("seconds".to_string())])
+        );
+
+        let mut scanner = Scanner::new("9hr1min");
+        let tokens = scanner.scan_tokens();
+        assert_eq!(
+            tokens,
+            Ok(vec![
+                Token::Number(9),
+                Token::Unit("hr".to_string()),
+                Token::Number(1),
+                Token::Unit("min".to_string()),
+            ])
+        );
+
+        let mut scanner = Scanner::new("712635 days");
+        let tokens = scanner.scan_tokens();
+        assert_eq!(
+            tokens,
+            Ok(vec![Token::Number(712635), Token::Unit("days".to_string())])
+        );
     }
 
     #[test]
